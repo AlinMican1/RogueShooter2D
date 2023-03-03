a0a770c8cda8613dcbfd3181ecb25738e791ad46
AlinMican <AlinMican2001@gmail.com>
2023-03-03 19:42:47 +0000

    Starts Work on line of sight algorithm

---
+++ Assets/Editor/FOV.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor (typeof (LineOfSight))]
+public class FOV : Editor
+{
+    private void OnSceneGUI()
+    {
+        LineOfSight fov = (LineOfSight)target;
+        Handles.color = Color.white;
+        Handles.DrawWireArc(fov.transform.position, Vector3.forward, Vector3.right, 360, fov.SightRadius);
+
+        Vector3 viewAngleA = fov.AngleFromDirection(-fov.Angle / 2, false);
+        Vector3 viewAngleB = fov.AngleFromDirection(fov.Angle / 2, false);
+        //Handles.DrawWireArc(fov.transform.position, Vector3.right, viewAngleA, fov.Angle, fov.SightRadius);
+        Handles.DrawLine(fov.transform.position, fov.transform.position + viewAngleA * fov.SightRadius);
+        Handles.DrawLine(fov.transform.position, fov.transform.position + viewAngleB * fov.SightRadius);
+    }
+}
+
---
+++ Assets/Scripts/Player/LineOfSight.cs
@@ -0,0 +1,105 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineOfSight : MonoBehaviour
+{
+    [Header("Sight Variables")]
+    public float Resolution;
+    public float SightRadius;
+    //Clamp values between 0 to 360.
+    [Range(0, 360)]
+    public float Angle;
+
+
+    public LayerMask obstacleMask;
+    public MeshFilter SightFilter;
+    Mesh SightMesh;
+    //Find the obstacle targets
+
+    private void Start()
+    {
+        SightMesh = new Mesh();
+        SightMesh.name = "Sight Mesh";
+        SightFilter.mesh = SightMesh;
+    }
+
+    //function for the angle we are facing in degrees which is converted into radiants.
+    public Vector3 AngleFromDirection(float angle, bool GlobalAngle)
+    {
+        if(GlobalAngle == false)
+        {
+            angle += transform.eulerAngles.y;
+        }
+        return new Vector3(Mathf.Sin(angle * Mathf.Deg2Rad), 0, Mathf.Cos(angle * Mathf.Deg2Rad));
+    }
+    
+    //Draw rays bewteen the FOV angle.
+    void DrawFOVRays()
+    {
+        int stepCount = Mathf.RoundToInt(Angle * Resolution);
+        float AngleSize = Angle / stepCount;
+        List<Vector3> SightPoints = new List<Vector3>();
+        for(int i = 0; i <= stepCount; i++)
+        {
+            float angle = transform.eulerAngles.y - Angle / 2 + AngleSize * i;
+            RayCastInfo newRayCast = RayCastSight(angle);
+            SightPoints.Add(newRayCast.EndPoint);
+        }
+        int VertexCount = SightPoints.Count + 1;
+        Vector3[] vertices = new Vector3[VertexCount];
+        int[] triangles = new int[(VertexCount - 2) * 3];
+
+        vertices[0] = Vector3.zero;
+        for (int i = 0;i < VertexCount - 1; i++)
+        {
+            vertices[i + 1] = transform.InverseTransformPoint(SightPoints[i]);
+            if(i<VertexCount - 2)
+            {
+                triangles[i * 3] = 0;
+                triangles[i * 3 + 1] = i + 1;
+                triangles[i * 3 + 2] = i + 2;
+            }
+            
+        }
+        SightMesh.Clear();
+        SightMesh.vertices = vertices;
+        SightMesh.triangles = triangles;
+        SightMesh.RecalculateNormals();
+    }
+    RayCastInfo RayCastSight(float globalAngle)
+    {
+        Vector3 direction = AngleFromDirection(globalAngle, true);
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position,direction,out hit, SightRadius, obstacleMask))
+        {
+            return new RayCastInfo(true, hit.distance, globalAngle, hit.point);
+        }
+        return new RayCastInfo(false, SightRadius, globalAngle, transform.position + direction * SightRadius);
+    }
+    //Handle rayCasts information
+    public struct RayCastInfo
+    {
+        public float DistanceRay;
+        public float AngleRay;
+        public bool RayHit;
+        public Vector3 EndPoint;
+
+        public RayCastInfo(bool RayHit, float DistanceRay, float AngleRay, Vector3 EndPoint)
+        {
+            this.DistanceRay = DistanceRay;
+            this.AngleRay = AngleRay;
+            this.RayHit = RayHit;
+            this.EndPoint = EndPoint;
+        }
+
+    }
+
+    //Update every frame, using LateUpdate to stop jitter for the line of sight.
+    private void LateUpdate()
+    {
+        DrawFOVRays();
+    }
+}
+
+
